8bdedcf7248e2fa0297b88a915a3ad956ef377ce
Lucas GT <lucas.g.theodooro@gmail.com>
2023-10-12 15:44:47 -0300

    correção

--- cotação do dolar/main.py
+++ cotação do dolar/main.py
@@ -1,17 +1,16 @@
 import requests
-from bs4 import BeautifulSoup as bs
-
-url = "https://www.google.com/search?q=cotacao+dolar"
+from bs4 import BeautifulSoup
+'''Aqui estamos pegando a cotação do dólar para real'''
+link = "https://www.google.com/search?q=cotacao+dolar"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"}
 
-
-requisicao = requests.get(url, headers=headers)
-
-site = bs(requisicao.text, "html.parser")
+requisicao = requests.get(link, headers=headers)
+site = BeautifulSoup(requisicao.text, "html.parser")
 
 
-titulo = site.find('title')
-print (titulo)
 
-pesquisa2 = site.find("input", class_="gLFyf")
-print(pesquisa2)
+titulo = site.find("title")
+print(titulo)
+
+cotacao_dolar = site.find("span", class_="SwHCTb")
+print(f'1 dolar é igual a {cotacao_dolar.get_text()}')
